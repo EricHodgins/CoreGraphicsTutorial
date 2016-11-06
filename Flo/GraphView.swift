e87f4c401449635c82f96ebbb41bd9e5543eefb0
Eric Hodgins <hodgins.e@gmail.com>
2016-11-06 01:59:49 -0500

    feat: Basic lines on graph setup for x, y points.

--- Flo/GraphView.swift
+++ Flo/GraphView.swift
@@ -15,8 +15,14 @@ class GraphView: UIView {
     // Gradients properties
     @IBInspectable var startColor: UIColor = UIColor.red
     @IBInspectable var endColor: UIColor = UIColor.green
+    
+    //Weekly sample data
+    var graphPoints: [Int] = [4, 2, 6, 4, 5, 8, 3]
 
     override func draw(_ rect: CGRect) {
+        let width = rect.width
+        let height = rect.height
+        
         // set up the background clipping area
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 8.0, height: 8.0))
         path.addClip()
@@ -39,7 +45,79 @@ class GraphView: UIView {
         let startPoint = CGPoint.zero
         let endPoint = CGPoint(x: 0, y: self.bounds.height)
         context!.drawLinearGradient(gradient!, start: startPoint, end: endPoint, options: CGGradientDrawingOptions(rawValue: 0))
+        
+        // Calculate the x point
+        
+        let margin: CGFloat = 20.0
+        let columnXPoint = { (column: Int) -> CGFloat in
+            //Calculate gap between points
+            let spacer = (width - margin*2 - 4) / CGFloat((self.graphPoints.count - 1))
+            var x: CGFloat = CGFloat(column) * spacer
+            x += margin + 2
+            
+            return x
+        }
+        
+        // Calculate the y point
+        let topBorder: CGFloat = 60
+        let bottomBorder: CGFloat = 50
+        let graphHeight = height - topBorder - bottomBorder
+        let maxValue = graphPoints.max()!
+        let columnYPoint = { (graphPoint: Int) -> CGFloat in
+            var y:CGFloat = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
+            y = graphHeight + topBorder - y // Flip the graph
+            
+            return y
+        }
+        
+        // Draw the line graph
+        UIColor.white.setFill()
+        UIColor.white.setStroke()
+        
+        // Set the points line
+        let graphPath = UIBezierPath()
+        // go to start fo line
+        graphPath.move(to: CGPoint(x: columnXPoint(0), y: columnYPoint(graphPoints[0])))
+        
+        // add points for each item in the graphPoints array at the correct (x, y) for the point
+        for i in 1..<graphPoints.count {
+            let nextPoint = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+            graphPath.addLine(to: nextPoint)
+        }
+        
+        graphPath.stroke()
     }
     
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
